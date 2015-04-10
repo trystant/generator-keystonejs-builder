32203a015633a8a2ea05363294afbdc28dcdd618
Mark Nyon <mark@grandkru.com>
2015-04-10 16:10:27 -0400

    Removed mock-fs

--- test/test-app.js
+++ test/test-app.js
@@ -4,27 +4,10 @@ var path = require('path');
 var assert = require('yeoman-generator').assert;
 var helpers = require('yeoman-generator').test;
 var os = require('os');
-var mock = require('mock-fs');
-
-/** Mock filesystem setup */
 
 
-var modelMockTemplate = "var keystone = require('keystone')," +
-" Types = keystone.Field.Types;" +
-" " +
-"/**" +
-" * <%= modelName %> Model" +
-" * ==========" +
-" */" +
-" " +
-"// Model creation" +
-" var <%= modelName %> = new keystone.List('<%= modelName %>', {" +
-" map: { name: 'title' }," +
-" autokey: { path: 'slug', from: 'title', unique: true }" +
-"});";
 
-
-describe('keystonejs-builder:model', function () {
+describe('keystonejs-builder:model User', function () {
   describe("on success", function() {
     beforeEach(function (done) {
       helpers.run(path.join(__dirname, '../model'))
