01f6983ba6733caf25164637a98ff5b25e266855
Mark Nyon <mark@grandkru.com>
2015-04-10 10:39:35 -0400

    Added failure case

--- test/test-app.js
+++ test/test-app.js
@@ -6,15 +6,30 @@ var helpers = require('yeoman-generator').test;
 var os = require('os');
 
 describe('keystonejs-builder:model', function () {
-  before(function (done) {
-    helpers.run(path.join(__dirname, '../model'))
-      .withArguments('User')
-      .withOptions({ skipInstall: true })
-      .withPrompts({ someOption: true })
-      .on('end', done);
+  describe("on success", function() {
+    beforeEach(function (done) {
+      helpers.run(path.join(__dirname, '../model'))
+        .withOptions({ skipInstall: true })
+        .withArguments('User')
+        .withPrompts({ someOption: true })
+        .on('end', done);
+    });
+    it('creates a model file', function () {
+      assert.file(['User.js']);
+    });
   });
 
-  it('creates a model file', function () {
-    assert.file(['User.js']);
+  describe("on failure", function() {
+    beforeEach(function (done) {
+      helpers.run(path.join(__dirname, '../model'))
+        .withOptions({ skipInstall: true })
+        .withArguments('Users')
+        .withPrompts({ someOption: true })
+        .on('end', done);
+    });
+    it('creates a model file', function () {
+      var assertionError = function() { throw new AssertionError("Wrong value"); };
+      assert.throws(assertionError);
+    });
   });
 });
